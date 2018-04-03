e3d429f35f8dd7b8d2ff8e1c7fa056decfc88495
Asya-brisk <emerald8colibri@gmail.com>
2018-04-03 17:05:19 +0300

    всплывашка

--- js/script.js
+++ js/script.js
@@ -58,6 +58,32 @@
     }
   });
 
+  var cartLink = document.querySelectorAll(".button-purchase");
+  var cartPopup = document.querySelector(".modal-cart");
+  var cartClose = cartPopup.querySelector(".modal-close");
+
+  for (var i = 0; i < cartLink.length; i++) {
+    cartLink[i].addEventListener('click', function(evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+    });
+  }
+
+  cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (cartPopup.classList.contains("modal-show")) {
+        cartPopup.classList.remove("modal-show");
+      }
+    }
+  });
+
+
   var mapLink = document.querySelector(".button-map");
   var mapPopup = document.querySelector(".modal-map");
   var mapClose = mapPopup.querySelector(".modal-close");
@@ -81,3 +107,5 @@
       }
     }
   });
+
+
